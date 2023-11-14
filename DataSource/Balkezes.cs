cf40a2ee47341a15aa21e05500326ab5599e1665
lajtosrob <lajtosrobert@freemail.hu>
2023-11-14 15:51:51 +0100

    Add project files.

---
+++ DataSource/Balkezes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balkezesek_concole.DataSource
+{
+    internal class Balkezes
+    {
+
+        string jatekosNeve;
+        DateOnly elsoDatum;
+        DateOnly utolsoDatum;
+        int jatekosSulya;
+        int jatekosMagassaga;
+
+        public Balkezes(string jatekosNeve, DateOnly elsoDatum, DateOnly utolsoDatum, int jatekosSulya, int jatekosMagassaga)
+        {
+            this.jatekosNeve = jatekosNeve;
+            this.elsoDatum = elsoDatum;
+            this.utolsoDatum = utolsoDatum;
+            this.jatekosSulya = jatekosSulya;
+            this.jatekosMagassaga = jatekosMagassaga;
+        }
+
+        public string JatekosNeve { get => jatekosNeve; set => jatekosNeve = value; }
+        public DateOnly ElsoDatum { get => elsoDatum; set => elsoDatum = value; }
+        public DateOnly UtolsoDatum { get => utolsoDatum; set => utolsoDatum = value; }
+        public int JatekosSulya { get => jatekosSulya; set => jatekosSulya = value; }
+        public int JatekosMagassaga { get => jatekosMagassaga; set => jatekosMagassaga = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using balkezesek_concole.DataSource;
+
+namespace balkezesek_concole
+{
+    internal class Program
+    {
+        static List<Balkezes> balkezesek = new List<Balkezes>();
+        static void Main(string[] args)
+        {
+
+            // 2. feladat
+
+            StreamReader sr = new StreamReader(".\\DataSource\\balkezesek.csv");
+
+            sr.ReadLine();
+
+            while(!sr.EndOfStream)
+            {
+                string[] line = sr.ReadLine().Split(";");
+
+                Balkezes adatsor = new Balkezes(
+                    line[0],
+                    DateOnly.ParseExact(line[1], "yyyy-MM-dd"),
+                    DateOnly.ParseExact(line[2], "yyyy-MM-dd"),
+                    int.Parse(line[3]),
+                    int.Parse(line[4])
+                    );
+
+                balkezesek.Add(adatsor);
+            }
+
+            sr.Close();
+
+            // 3. feladat 
+
+            Console.WriteLine($"3. feladat: {balkezesek.Count()}");
+
+            // 4. feladat
+
+            List<Balkezes> palyaraLepes1999Elott = balkezesek.Where(x => x.UtolsoDatum >= DateOnly.Parse("1999. 10. 01") && x.UtolsoDatum <= DateOnly.Parse("1999. 10. 31.")).ToList();
+
+            Console.WriteLine("4. feladat: ");
+
+            palyaraLepes1999Elott.ForEach(x => Console.WriteLine($"\t{x.JatekosNeve},{Math.Round((x.JatekosMagassaga) * 2.54, 1)} cm"));
+
+
+            int evszam = 0;
+
+            while(true)
+            {
+                Console.Write("Kérek egy 1990 és 1999 közötti évszámot!");
+
+                evszam = Convert.ToInt32(Console.ReadLine());
+                if(evszam > 1999 || evszam < 1990 )
+                {
+                    Console.Write("Hibás adat!");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // 6. feladat
+
+            var atlagsulyAzAdottEvben = balkezesek.Where(x => x.ElsoDatum.Year >= evszam && x.UtolsoDatum.Year <= evszam).Average(x => x.JatekosSulya);
+
+            Console.WriteLine($"6. feladat: {Math.Round(atlagsulyAzAdottEvben ,2)} font");
+
+        }
+    }
+}
